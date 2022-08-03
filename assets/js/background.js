e0b2214ccd4cb287f48d6431e6cc05d43a5e38af
Jepf <jepfarragona@gmail.com>
2022-08-03 23:59:32 +0800

    initial

--- assets/js/background.js
+++ assets/js/background.js
@@ -728,7 +728,7 @@ let bloomFramebuffers = [];
 let sunrays;
 let sunraysTemp;
 
-let ditheringTexture = createTextureAsync('js/background.png');
+let ditheringTexture = createTextureAsync('assets/js/background.png');
 
 const blurProgram = new Program(blurVertexShader, blurShader);
 const copyProgram = new Program(baseVertexShader, copyShader);
