ea2625ea782f75d9c1717daee17929ade1bf9163
Jepf <jepfarragona@gmail.com>
2022-08-07 11:25:59 +0800

    initial

--- assets/js/background.js
+++ assets/js/background.js
@@ -728,7 +728,7 @@ let bloomFramebuffers = [];
 let sunrays;
 let sunraysTemp;
 
-let ditheringTexture = createTextureAsync('assets/js/background.png');
+let ditheringTexture = createTextureAsync('https://undeadmu.net/themes/def_theme/images/overlay-1.png');
 
 const blurProgram = new Program(blurVertexShader, blurShader);
 const copyProgram = new Program(baseVertexShader, copyShader);
